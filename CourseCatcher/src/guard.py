cec680e4f89493c75b1eee8c7d54ab33ac88c8c2
cxjyxxme <498731903@qq.com>
2017-02-24 15:49:07 +0800

    fix some bug for fit idcode

--- CourseCatcher/src/guard.py
+++ CourseCatcher/src/guard.py
@@ -10,24 +10,24 @@ for user in users:
 	action = UserAction(user.id)
 	action.get_cookie()
 
+times = 0
 while True:
 	users = User.objects.all()
 	user_actions = []
 	for user in users:
 		user_actions.append(UserAction(user.id))
 	
-	times = 0
 	for action in user_actions:
 		print '[' + str(action.id) + ' : select course]'
 		if not action.select_course(""):
 			print '[' + str(action.id) + ' : login]'
 			if not action.login():
 				print "[not enough codes]"
-		times += 1
-		if times % 10 == 0:
+	times += 1
+	if times % 10 == 0:
+		for action in user_actions:
 			action.check_course()
-	time.sleep(0.2)
-	
+	time.sleep(2)
 	
 	#t0 = datetime.datetime.now()
 	#print ((datetime.datetime.now() - t0)).total_seconds()
